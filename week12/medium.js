fe5aa18fc24b50d8cf6963f07bb9347d079c88a2
Natalie Bruns <nataliebruns@Natalies-MacBook-Air.local>
2022-11-22 09:36:13 -0500

    medium is massage

---
+++ week12/medium.js
@@ -0,0 +1,24 @@
+
+var paragraph = document.getElementById("numbers-array")
+var numbers = [8,"-", 4, "="];
+var operations = ["+","-","*", "/"]
+var newnumbers = [1,2,3,4,5,6,7,8,9]
+var operation = operations[Math.floor(Math.random()*operations.length)];
+var newnumber = newnumbers[Math.floor(Math.random()*newnumbers.length)];
+paragraph.innerHTML = numbers.join(" ");
+
+function newNumber(){
+  var operation = operations[Math.floor(Math.random()*operations.length)];
+  var newnumber = newnumbers[Math.floor(Math.random()*newnumbers.length)];
+  if (numbers.length < 16){
+    numbers.unshift(operation);
+    numbers.unshift(newnumber);
+  } else if (numbers.length >= 16){
+    numbers = [8,"-", 4, "="]
+    var buttontitle = document.getElementById("button");
+    buttontitle.innerHTML = "Click for answer!";
+  }
+  paragraph.innerHTML = numbers.join(' ');
+  var buttontitle = document.getElementById("button");
+  buttontitle.innerHTML = "try again!";
+}
